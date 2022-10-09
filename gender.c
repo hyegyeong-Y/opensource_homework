76b6df3a4708d1289376a417cb2d86d64cc36f1a
ito <yanghk7689@naver.com>
2022-10-09 14:08:32 +0900

    c언어(3)
    while문을 사용하여 문자 하나 입력 받아서, 남자, 여자 출력하는 프로그램

---
+++ gender.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+    char ch;
+ 
+ 
+    printf ("문자를 입력하세요 : ");
+    scanf ("%c", &ch);
+ 
+ 
+    if (ch == 'M' || ch == 'm' )
+        printf ("남자");
+ 
+    if (ch == 'F' || ch == 'f' )
+        printf ("여자");
+ 
+ 
+    return 1;
+ }
